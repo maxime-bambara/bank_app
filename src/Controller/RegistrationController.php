199d5ba9b5a7ae545ebc6b7d0166161eccdb6eb3
maxime bambara <maxime.f.bambara@gmail.com>
2021-04-16 18:21:26 +0200

    fix banker registration bug

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -89,7 +89,7 @@ class RegistrationController extends AbstractController
             );
         }
 
-        return $this->render('registration/register.html.twig', [
+        return $this->render('registration/banker.register.html.twig', [
             'registrationForm' => $form->createView(),
         ]);
     }
